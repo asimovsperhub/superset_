6b8f09f8038f8159d5e63366961670e10182fa42
asimov <1019022410@qq.com>
2023-08-23 17:17:18 +0800

    fix: config

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -22,54 +22,42 @@
 #
 import logging
 import os
-from typing import Optional, Any
 
 from cachelib.file import FileSystemCache
 from celery.schedules import crontab
 
 logger = logging.getLogger()
-SUPERSET_LOG_VIEW = True
-if "SUPERSET_HOME" in os.environ:
-    DATA_DIR = os.environ["SUPERSET_HOME"]
-else:
-    DATA_DIR = os.path.expanduser("~/.superset")
 
+DATABASE_DIALECT = os.getenv("DATABASE_DIALECT")
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_PORT = os.getenv("DATABASE_PORT")
+DATABASE_DB = os.getenv("DATABASE_DB")
 
-def get_env_variable(var_name: str, default: Optional[str] = None) -> str:
-    """Get the environment variable or raise exception."""
-    try:
-        return os.environ[var_name]
-    except KeyError:
-        if default is not None:
-            return default
-        else:
-            error_msg = "The environment variable {} was missing, abort...".format(
-                var_name
-            )
-            raise OSError(error_msg)
-
-
-DATABASE_DIALECT = get_env_variable("DATABASE_DIALECT")
-DATABASE_USER = get_env_variable("DATABASE_USER")
-DATABASE_PASSWORD = get_env_variable("DATABASE_PASSWORD")
-DATABASE_HOST = get_env_variable("DATABASE_HOST")
-DATABASE_PORT = get_env_variable("DATABASE_PORT")
-DATABASE_DB = get_env_variable("DATABASE_DB")
+EXAMPLES_USER = os.getenv("EXAMPLES_USER")
+EXAMPLES_PASSWORD = os.getenv("EXAMPLES_PASSWORD")
+EXAMPLES_HOST = os.getenv("EXAMPLES_HOST")
+EXAMPLES_PORT = os.getenv("EXAMPLES_PORT")
+EXAMPLES_DB = os.getenv("EXAMPLES_DB")
 
 # The SQLAlchemy connection string.
-SQLALCHEMY_DATABASE_URI = "{}://{}:{}@{}:{}/{}".format(
-    DATABASE_DIALECT,
-    DATABASE_USER,
-    DATABASE_PASSWORD,
-    DATABASE_HOST,
-    DATABASE_PORT,
-    DATABASE_DB,
+SQLALCHEMY_DATABASE_URI = (
+    f"{DATABASE_DIALECT}://"
+    f"{DATABASE_USER}:{DATABASE_PASSWORD}@"
+    f"{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_DB}"
 )
 
-REDIS_HOST = get_env_variable("REDIS_HOST")
-REDIS_PORT = get_env_variable("REDIS_PORT")
-REDIS_CELERY_DB = get_env_variable("REDIS_CELERY_DB", "0")
-REDIS_RESULTS_DB = get_env_variable("REDIS_RESULTS_DB", "1")
+SQLALCHEMY_EXAMPLES_URI = (
+    f"{DATABASE_DIALECT}://"
+    f"{EXAMPLES_USER}:{EXAMPLES_PASSWORD}@"
+    f"{EXAMPLES_HOST}:{EXAMPLES_PORT}/{EXAMPLES_DB}"
+)
+
+REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_PORT = os.getenv("REDIS_PORT", "6379")
+REDIS_CELERY_DB = os.getenv("REDIS_CELERY_DB", "0")
+REDIS_RESULTS_DB = os.getenv("REDIS_RESULTS_DB", "1")
 
 RESULTS_BACKEND = FileSystemCache("/app/superset_home/sqllab")
 
@@ -125,52 +113,3 @@ try:
     )
 except ImportError:
     logger.info("Using default Docker config...")
-
-
-
-# Allowed format types for upload on Database view
-EXCEL_EXTENSIONS = {"xlsx", "xls"}
-CSV_EXTENSIONS = {"csv", "tsv", "txt"}
-COLUMNAR_EXTENSIONS = {"parquet", "zip"}
-ALLOWED_EXTENSIONS = {*EXCEL_EXTENSIONS, *CSV_EXTENSIONS, *COLUMNAR_EXTENSIONS}
-
-# Optional maximum file size in bytes when uploading a CSV
-CSV_UPLOAD_MAX_SIZE = None
-
-# CSV Options: key/value pairs that will be passed as argument to DataFrame.to_csv
-# method.
-# note: index option should not be overridden
-CSV_EXPORT = {"encoding": "utf-8"}
-
-# Excel Options: key/value pairs that will be passed as argument to DataFrame.to_excel
-# method.
-# note: index option should not be overridden
-EXCEL_EXPORT: dict[str, Any] = {}
-
-# Specify the App icon
-APP_ICON = "/static/assets/images/superset-logo-horiz.png"
-
-
-BABEL_DEFAULT_LOCALE = "zh"
-
-from superset.utils.logging_configurator import DefaultLoggingConfigurator
-
-# Default configurator will consume the LOG_* settings below
-LOGGING_CONFIGURATOR = DefaultLoggingConfigurator()
-
-# Console Log Settings
-
-LOG_FORMAT = "%(asctime)s:%(levelname)s:%(name)s:%(message)s"
-LOG_LEVEL = "DEBUG"
-
-# ---------------------------------------------------
-# Enable Time Rotate Log Handler
-# ---------------------------------------------------
-# LOG_LEVEL = DEBUG, INFO, WARNING, ERROR, CRITICAL
-
-ENABLE_TIME_ROTATE = False
-TIME_ROTATE_LOG_LEVEL = "DEBUG"
-FILENAME = os.path.join(DATA_DIR, "superset.log")
-ROLLOVER = "midnight"
-INTERVAL = 1
-BACKUP_COUNT = 30
